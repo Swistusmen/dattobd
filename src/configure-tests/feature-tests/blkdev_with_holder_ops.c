0f8bcf9661180786897d59c2c2c893dc6951e719
datto <datto@localhost.localdomain>
2024-01-24 04:48:55 -0500

    adding-proper-test

---
+++ src/configure-tests/feature-tests/blkdev_with_holder_ops.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
+ * Copyright (C) 2024 Datto Inc.
+ */
+
+#include "includes.h"
+
+MODULE_LICENSE("GPL");
+
+static inline void dummy(void){
+
+	struct block_device* dev;
+    char* path;
+    dev=blkdev_get_by_path(path,FMODE_READ,NULL,NULL);
+}
